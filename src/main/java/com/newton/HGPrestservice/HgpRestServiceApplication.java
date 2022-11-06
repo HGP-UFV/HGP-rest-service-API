a91cff47c454b9027aa97fca6538db57947c3192
HGP-UFV <9004673@alumnos.ufv.es>
2022-11-06 17:01:15 +0100

    Primer Commit. Archivos generados por SpringBoot

---
+++ src/main/java/com/newton/HGPrestservice/HgpRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.newton.HGPrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgpRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HgpRestServiceApplication.class, args);
+	}
+
+}
